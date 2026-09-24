a0292bd6298a0cd17447a4f093ac3d0e090fc704
Mnzo <81849467+Mnzo@users.noreply.github.com>
2026-09-24 17:43:40 +0200

    Added pixelate shader + created player scene/spawn and main script

---
+++ Levels/Main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const PLAYER_CONTROLLER = preload("res://Core/Player/Player.tscn")
+
+func _ready() -> void:
+	spawn_player()
+
+func spawn_player() -> void:
+	var spawn_point: Node3D = %SpawnPoint
+	var entities: Node3D = %Entities
+	var new_player = PLAYER_CONTROLLER.instantiate()
+	
+	new_player.position = spawn_point.global_position
+	entities.add_child(new_player)
