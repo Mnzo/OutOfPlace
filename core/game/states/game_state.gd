fa9ce640ef8e328cfaf91fe764c9f1f15abfac8d
Mnzo <81849467+Mnzo@users.noreply.github.com>
2026-09-25 18:22:08 +0200

    Entire renaming to snake_case for on-disk files

---
+++ core/game/states/game_state.gd
@@ -0,0 +1,5 @@
+@abstract
+class_name GameState
+extends State
+
+@onready var game_mode: GameMode = owner as GameMode
--- managers/signals_manager.gd
+++ managers/signals_manager.gd
@@ -1,4 +1,4 @@
 extends Node
 
 @warning_ignore("unused_signal")
-signal dice_session_requested()
+signal dice_session_requested
